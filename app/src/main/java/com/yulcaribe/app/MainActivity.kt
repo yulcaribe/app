10be98e6bf358c3bd332671927529dd6a8681863
yulcaribe <mehmetisaacar47@gmail.com>
2026-09-26 16:41:20 +0300

    Wire appearance state and background refresh

--- app/src/main/java/com/yulcaribe/app/MainActivity.kt
+++ app/src/main/java/com/yulcaribe/app/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import org.maplibre.android.MapLibre
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MapLibre.getInstance(this)
+        scheduleWeatherRefresh(this)
         enableEdgeToEdge()
         setContent {
-            YulCaribeTheme {
-                YulCaribeApp()
+            var appearance by remember { mutableStateOf(loadAppearanceMode(this)) }
+            YulCaribeTheme(appearance = appearance) {
+                YulCaribeApp(onAppearanceChanged = { appearance = it })
             }
         }
     }
