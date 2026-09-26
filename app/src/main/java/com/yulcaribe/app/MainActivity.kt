4b65ba2a51dbe6574e882502a54cca7f0d356e47
yulcaribe <mehmetisaacar47@gmail.com>
2026-09-26 16:49:33 +0300

    Switch launcher to finalized vNext app shell

--- app/src/main/java/com/yulcaribe/app/MainActivity.kt
+++ app/src/main/java/com/yulcaribe/app/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             var appearance by remember { mutableStateOf(loadAppearanceMode(this)) }
             YulCaribeTheme(appearance = appearance) {
-                YulCaribeApp(onAppearanceChanged = { appearance = it })
+                YulCaribeAppVNext(onAppearanceChanged = { appearance = it })
             }
         }
     }
